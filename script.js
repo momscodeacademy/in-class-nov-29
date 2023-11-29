d305c3c09af91502fefe4bed2041ecd1860d1eed
Alicia Wood <alicia.crowder@gmail.com>
2023-11-29 15:20:31 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function getBooksByGenre(books, genre){
+
+}
+
+
+let myBooks = [
+    {
+        title: "10x is Easier than 2x",
+        author: "Benjamin Hardy",
+        genre: "non-fiction"
+    },
+    {
+        title: "Murder on the Orient Express",
+        author: "Agatha Christie",
+        genre: "mystery"
+    },
+    {   
+        title: "Harry Potter",   
+        author: "J. K. Rowling",
+        genre: "fantasy"
+    },
+    {
+        title: "The Big Leap",
+        author: "Gay Hendricks",
+        genre: "non-fiction"
+    }
+    
+];
+
+getBooksByGenre(myBooks, "non-fiction");
+getBooksByGenre(myBooks, "mystery");
